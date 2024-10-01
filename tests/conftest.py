4961edbb2705d93807bfa34b3445dd4ff56c4133
Ekaterina Kaiuda <ekaterina@192.168.1.3>
2024-09-30 21:37:35 -0300

    Changed conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,35 @@
 import pytest
+
 from selene import browser
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 from utils import attach
-from dotenv import load_dotenv
-import os
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_configuration():
-    options = Options()
-    selenoid_capabilities = {
-        'browserName': 'chrome',
-        'browserVersion': '126.0',
-        'selenoid:options': {
-            'enableVNC': True,
-            'enableVideo': True
-        }
-    }
-    selenoid_login = os.getenv("SELENOID_LOGIN")
-    selenoid_pass = os.getenv("SELENOID_PASS")
-    selenoid_url = os.getenv("SELENOID_URL")
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
-        options=options)
-
-    browser.config.driver = driver
+@pytest.fixture(scope="function", autouse=True)
+def setup_browser(request):
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
     yield
+
+    browser.quit()
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
@@ -39,7 +37,3 @@ def browser_configuration():
     attach.add_video(browser)
 
     browser.quit()
-
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
